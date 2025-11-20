6e9a1d6ada27cb342bad3b17b3d182dbeba25c8b
rony <zakiwriters@gmail.com>
2025-11-20 10:44:53 +0100

    Added frontend

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+// Serve files from the public folder
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+// Use Render's PORT environment variable
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
