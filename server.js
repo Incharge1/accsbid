60d73b533a1aa1ecdab145583e29e72f59e75783
rony <zakiwriters@gmail.com>
2025-11-20 11:32:14 +0100

    Updated server.js with routes

--- server.js
+++ server.js
@@ -4,14 +4,29 @@ const app = express();
 // Serve files from the public folder
 app.use(express.static("public"));
 
+// Home page
 app.get("/", (req, res) => {
-  res.send("Server is running!");
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+// Login page
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/public/login.html");
+});
+
+// Signup page
+app.get("/signup", (req, res) => {
+    res.sendFile(__dirname + "/public/signup.html");
+});
+
+// Dashboard page
+app.get("/dashboard", (req, res) => {
+    res.sendFile(__dirname + "/public/dashboard.html");
 });
 
 // Use Render's PORT environment variable
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
