a9ebb1277457d75b15a969204987b43b66ea3da1
rony <zakiwriters@gmail.com>
2025-11-20 11:47:09 +0100

    Fix /api/test route

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
-
-// Serve files from the public folder
-app.use(express.static("public"));
-
-// Home page
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
-// Login page
-app.get("/login", (req, res) => {
-    res.sendFile(__dirname + "/public/login.html");
-});
-
-// Signup page
-app.get("/signup", (req, res) => {
-    res.sendFile(__dirname + "/public/signup.html");
-});
-
-// Dashboard page
-app.get("/dashboard", (req, res) => {
-    res.sendFile(__dirname + "/public/dashboard.html");
-});
-
-// Use Render's PORT environment variable
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Server is running" });
+});
+
+// Root
+app.get('/', (req, res) => {
+  res.send('ACCSBID backend is working...');
+});
+
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
